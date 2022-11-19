86c014b99de9c0282fbe79c34c956b43fd0d5078
Rama <ramiroquesadaperez@gmail.com>
2022-11-18 22:39:52 -0300

    advances

--- app.js
+++ app.js
@@ -11,17 +11,50 @@ inputs.forEach((input) => {
 });
 
 
-let password = document.getElementById("passwd");
-let password2 = document.getElementById("passwd2").value;
 
-// no puedo leer el password desde el js
+let form = document.getElementById("registro");
+let textoDer = document.getElementById("textoDer");
+let loggedIn = document.getElementById("loggedIn");
+let nombre = document.getElementById("nombre")
+
 
 const checkPasswd = ()=>{
-  console.log(password);
+  let password1 = document.getElementById("passwd").value;
+  let password2 = document.getElementById("passwd2").value;
+  if (password1 === password2){
+    return true;
+  }
+  else {return false};
+  
 }
 
 
 const submitButton = document.getElementById("submit");
-submitButton.addEventListener("submit", checkPasswd);
+submitButton.addEventListener("click", (e)=> {
+
+  e.preventDefault();
+
+ let pass =  checkPasswd();
+
+  if (pass){   
+    
+    form.classList.add("hidden")
+    textoDer.classList.add("hidden")
+    
+    loggedIn.innerHTML = `Bienvenido ${nombre.value}`
+    loggedIn.classList.remove("hidden");
+
+
+
+
+  }
+  else{
+    
+        
+    return
+  }
+   
+  }
+);
 
 
