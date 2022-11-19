07ce34251244cbae9d0998bd387f30db51dae5fe
Rama <ramiroquesadaperez@gmail.com>
2022-11-18 21:39:59 -0300

    advances

--- app.js
+++ app.js
@@ -1,14 +1,27 @@
 const inputs = document.querySelectorAll("input, select, textarea");
 
-inputs.forEach(input => {
-  
-    
+inputs.forEach((input) => {
   input.addEventListener(
     "invalid",
-    event => {
+    (event) => {
       input.classList.add("error");
     },
     false
   );
 });
+
+
+let password = document.getElementById("passwd");
+let password2 = document.getElementById("passwd2").value;
+
+// no puedo leer el password desde el js
+
+const checkPasswd = ()=>{
+  console.log(password);
+}
+
+
+const submitButton = document.getElementById("submit");
+submitButton.addEventListener("submit", checkPasswd);
+
 
